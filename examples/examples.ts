7282b3a08f2cc6e496ea0198ee8cbcbedcc8e658
shapirov103 <shapirov@amazon.com>
2023-04-24 22:06:09 -0400

    added example with cross stack references

--- examples/examples.ts
+++ examples/examples.ts
@@ -4,6 +4,9 @@ import * as kms  from 'aws-cdk-lib/aws-kms';
 import * as bp from '../lib';
 import * as bcrypt from 'bcrypt';
 import { KubernetesVersion } from 'aws-cdk-lib/aws-eks';
+import { Stack } from 'aws-cdk-lib';
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
+import { DirectVpcProvider } from '../lib';
 
 /**
  * You can run these examples with the following command:
@@ -46,6 +49,40 @@ builder()
     .addOns(buildArgoBootstrap())
     .build(app, 'argo-blueprint1');
 
+
+class OtherStack extends Stack {
+
+    public readonly vpc: Vpc;
+    public readonly securityGroupWeb: ec2.ISecurityGroup;
+
+    constructor() {
+        super(app, "other-stack", {
+            env: {
+                account: process.env.CDK_DEFAULT_ACCOUNT,
+                region: process.env.CDK_DEFAULT_REGION
+            }
+        });
+        this.vpc = new Vpc(this, "central-vpc");
+        this.securityGroupWeb = new ec2.SecurityGroup(this, 'service-sg-web', {
+            vpc: this.vpc,
+            securityGroupName: 'sg-web',
+            description: 'Shared CDK-managed Security Group for HTTP(S) traffic'
+        });
+        this.securityGroupWeb.connections.allowFrom(ec2.Peer.ipv4('169.132.88.0/22'), ec2.Port.tcp(443), 'Allow HTTPS connections from internal network');
+    }
+}
+
+const otherStack = new OtherStack();
+
+builder()
+    .clusterProvider(new bp.MngClusterProvider({
+        ...publicCluster,
+        securityGroup: otherStack.securityGroupWeb 
+    }))
+    .resourceProvider(bp.GlobalResources.Vpc, new DirectVpcProvider(otherStack.vpc))
+    .account(process.env.CDK_DEFAULT_ACCOUNT)
+    .region(process.env.CDK_DEFAULT_REGION)
+    .build(app, "cross-stack-ref-blueprint");
 
 function buildArgoBootstrap() {
     return new bp.addons.ArgoCDAddOn({
